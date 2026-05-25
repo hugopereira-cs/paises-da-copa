a4d6c39099d98e9d1e7ac5e3451fb68bfb0fbd36
Hugo Pereira <hugoper_cs@yahoo.com>
2026-05-25 15:24:00 -0300

    feat: add simple Footer with LinkedIn link

---
+++ src/components/Footer.tsx
@@ -0,0 +1,20 @@
+export function Footer() {
+	return (
+		<footer className="p-8 text-center text-app-primary opacity-60">
+			<p 
+				style={{ fontSize: '12px' }} 
+				className="uppercase tracking-[0.2em]"
+			>
+			  2026 - Desenvolvido por{' '}
+				<a
+					href="https://www.linkedin.com/in/hugopereiradev/"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="font-bold transition-colors"
+				>
+					Hugo Pereira
+				</a>				
+			</p>
+		</footer>
+	);
+}
