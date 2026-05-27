3b34092f8e068cf2448d4a190cbf0f012fb2e595
Hugo Pereira <hugoper_cs@yahoo.com>
2026-05-27 14:33:38 -0300

    style: fix CountrySection styles

--- src/components/CountrySection.tsx
+++ src/components/CountrySection.tsx
@@ -30,7 +30,7 @@ export function CountrySection() {
 
 	return (
 		<>
-			<section className="mx-auto grid max-w-[1400px] grid-cols-1 justify-items-center gap-6 p-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+			<section className="mx-auto grid max-w-350 grid-cols-1 justify-items-center gap-6 p-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
 				{countries.map((country) => (
 					<CountryCard
 						key={country.id}
