56a3fc6621efe6241f0a9af7c1ac06a980f16d9e
Hugo Pereira <hugoper_cs@yahoo.com>
2026-05-28 16:50:40 -0300

    style: add letter spacing to TextSection title

--- src/components/TextSection.tsx
+++ src/components/TextSection.tsx
@@ -1,3 +1,12 @@
 export function TextSection() {
-  return <h1 className="text-4xl text-center m-4 font-bold text-app-primary">Paises da Copa</h1>
+	return (
+		<>
+			<h1 className="text-4xl text-center tracking-wide m-4 font-bold text-app-primary">
+				Paises da Copa
+			</h1>
+      <p className="text-lg text-center m-4 text-app-secondary">
+        Clique sobre um país para ver suas informações
+      </p>
+		</>
+	);
 }
